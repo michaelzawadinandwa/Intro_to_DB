3a871fefd5778620347c92df4eae9017e1c16e1a
michaelzawadinandwa <michaelnandwa13@gmail.com>
2025-12-12 13:40:04 +0300

    add

--- task_4.sql
+++ task_4.sql
@@ -1,17 +1,13 @@
 -- Script to print full description of the books table
--- Database: alx_book_store
-
 SELECT 
-    COLUMN_NAME AS 'Field',
-    COLUMN_TYPE AS 'Type',
-    IS_NULLABLE AS 'Null',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_SCHEMA = 'alx_book_store'
-    AND TABLE_NAME = 'books'
-ORDER BY 
-    ORDINAL_POSITION;
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'books';
