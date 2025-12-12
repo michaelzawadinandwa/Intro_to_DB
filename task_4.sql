2077758abcb942067932317596d779571c08c478
michaelzawadinandwa <michaelnandwa13@gmail.com>
2025-12-12 13:50:33 +0300

    commit new change

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,7 @@
 -- task_4.sql
 -- Script to print full description of the books table without using DESCRIBE
-
+-- Script to print full description of the books table using SHOW
+SHOW COLUMNS FROM books;
 ANALYZE TABLE books;
 
 SELECT 
@@ -15,3 +16,18 @@ FROM
 WHERE 
     TABLE_SCHEMA = DATABASE()
     AND TABLE_NAME = 'books';
+
+SELECT
+    COLUMN_NAME AS `Field`,
+    COLUMN_TYPE AS `Type`,
+    COLLATION_NAME AS `Collation`,
+    IS_NULLABLE AS `Null`,
+    COLUMN_KEY AS `Key`,
+    COLUMN_DEFAULT AS `Default`,
+    EXTRA AS `Extra`,
+    PRIVILEGES AS `Privileges`,
+    COLUMN_COMMENT AS `Comment`
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'books'
+ORDER BY ORDINAL_POSITION;
