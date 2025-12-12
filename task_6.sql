da9b72a22855fd749a7c28aedda28ac8c5f3e460
michaelzawadinandwa <michaelnandwa13@gmail.com>
2025-12-12 19:45:31 +0300

    add task 6 changes to file

--- task_6.sql
+++ task_6.sql
@@ -1,12 +1,9 @@
--- Check if customers already exist and insert only if they don't
-INSERT INTO customer (customer_id, customer_name, email, address)
-SELECT 2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'
-WHERE NOT EXISTS (SELECT 1 FROM customer WHERE customer_id = 2);
+-- task_6.sql
+-- Insert multiple customers into the `customer` table for alx_book_store
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-SELECT 3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'
-WHERE NOT EXISTS (SELECT 1 FROM customer WHERE customer_id = 3);
+VALUES
+	(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+	(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+	(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
 
-INSERT INTO customer (customer_id, customer_name, email, address)
-SELECT 4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.'
-WHERE NOT EXISTS (SELECT 1 FROM customer WHERE customer_id = 4);
