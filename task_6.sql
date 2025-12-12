c85a07661e8b75deedbd2db7d5c4638862256d9b
michaelzawadinandwa <michaelnandwa13@gmail.com>
2025-12-12 19:22:47 +0300

    add new changes

--- task_6.sql
+++ task_6.sql
@@ -1,10 +1,6 @@
--- task_6.sql
--- Script to insert multiple rows in the customer table
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES
+	(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+	(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+	(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
 
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(3, 'Obed Ehoneah', 'oebed@sandtech.com', '125 Happiness Ave.');
-
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
