9ce808be3ec4ecfd752d068e13eaf774fa31c313
michaelzawadinandwa <michaelnandwa13@gmail.com>
2025-12-12 14:38:01 +0300

    commit new change

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,16 @@
 -- task_4.sql
 -- Script to print full description of the books table
-SHOW COLUMNS FROM books;
+ANALYZE TABLE books;
+
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE()
+    AND TABLE_NAME = 'books';
