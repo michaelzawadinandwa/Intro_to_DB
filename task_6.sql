2582e22562a110bfb1a85a571c61d9735c64cba5
michaelzawadinandwa <michaelnandwa13@gmail.com>
2025-12-13 20:20:28 +0300

    commit new and final change

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,7 @@
 -- task_6.sql
--- Insert multiple customers into the `customer` table for alx_book_store
+-- Insert multiple customers into the CUSTOMERS table for alx_book_store
 
-INSERT INTO customer (customer_id, customer_name, email, address)
+INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
 VALUES
 	(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 	(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
