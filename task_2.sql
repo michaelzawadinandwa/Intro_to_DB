8ef39cb2b69d12da47cc0884ba17722794575b3a
michaelzawadinandwa <michaelnandwa13@gmail.com>
2025-12-27 14:25:27 +0300

    add fix to task 2

--- task_2.sql
+++ task_2.sql
@@ -33,6 +33,7 @@ CREATE TABLE IF NOT EXISTS CUSTOMERS (
 -- ============================
 -- Orders Table
 -- ============================
+-- FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 CREATE TABLE IF NOT EXISTS ORDERS (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
