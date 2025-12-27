2c5cc839de9b7d48f8f07893a6c54b840353821f
michaelzawadinandwa <michaelnandwa13@gmail.com>
2025-12-27 14:32:18 +0300

    commit task 4 changes

--- task_4.sql
+++ task_4.sql
@@ -1,14 +1,14 @@
 -- task_4.sql
 -- Script to print full description of the books table
 SELECT
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
+    COLUMN_NAME AS `Field`,
+    COLUMN_TYPE AS `Type`,
+    IS_NULLABLE AS `Null`,
+    COLUMN_KEY AS `Key`,
+    COLUMN_DEFAULT AS `Default`,
+    EXTRA AS `Extra`
 FROM
     INFORMATION_SCHEMA.COLUMNS
 WHERE
-    TABLE_SCHEMA = DATABASE()
-    AND TABLE_NAME = 'BOOKS';
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
